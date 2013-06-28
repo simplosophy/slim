22bd85974bf4aee698df9f9ef40f0019cec70223
simplosophy <simplosophy@gmail.com>
2013-06-28 23:59:09 +0800

    add sMap,sReduce,sLoads

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 slim
 ====
 
-my handy javascript lib
+my handy javascript lib, which may help writing single-line code.  **warning Array and Object namespace would be polluted (`slim` members' name start with `s` and an upper case character follows).**
+*****************************
+**KEEP AN ARRAY IN YOUR MIND**
+*****************************
+
+#Object Methods
+## 1.sLog
+log the object to console, eg.
+
+```
+[1,2,{a:1}].sLog();
+```
+## 2.sDumps
+if in node.js, serialize the invoking object string to file, 
+otherwise, dumps string to console.
+eg.
+
+```
+[1,2,3].sDumps('a.json');//a 'a.json' file would be created in node.js
+```
+## 3.sLoads
+**node.js only**, load js object from file.
+eg.
+
+```
+Object.sLoads('a.json');
+```
+
+#Array Methods
+##1. sFilter
+filtering an array for given condition(Lambda string or Funtion object),eg.
+
+```
+[1,2,3].sFilter('$>1&&$<3');//result is [2]
+[{a:1},{a:2},{a:3}].sFilter('$.a>1');//result is [{a:2},{a:3}]
+[1,2,3].sFilter(function(e){return e>1});//result is [2,3]
+```
+##2. sMap
+do something with each element of the array, and return a new one(Lambda string or Funtion object),eg.
+
+```
+[1,2,{a:1}].sMap('$,i=>{"index":i,"value":$}');//result is :
+	[ { index: 0, value: 1 },
+	  { index: 1, value: 2 },
+	  { index: 2, value: { a: 1 } } ]
+[1,2,3].sFilter(function(e){return e+1});//result is [2,3,4]
+```
+##2. sReduce
+reduce an array, and return one object(Lambda string or Funtion object),eg.
+
+```
+[1,2,3].sReduce('a,b=>a+b'); //result is : 6
+```
--- slim.js
+++ slim.js
@@ -1,183 +1,205 @@
 /**
- * User: flying
+ * Author: FlyingShang
  * Date: 13-6-26
  * Time: PM4:53
  */
 
+(function (){
 
 // static functions
-var Functions =
-{
-  Identity: function (x) { return x; },
-  True: function () { return true; },
-  Blank: function () { }
-}
+  var Functions =
+  {
+    Identity: function (x) { return x; },
+    True: function () { return true; },
+    Blank: function () { }
+  }
 
 // static const
-var Types =
-{
-  Boolean: typeof true,
-  Number: typeof 0,
-  String: typeof "",
-  Object: typeof {},
-  Undefined: typeof undefined,
-  Function: typeof function () { }
-}
-
-// static utility methods
-var Utils =
-{
-  // Create anonymous function from lambda expression string
-  CreateLambda: function (expression)
+  var Types =
   {
-    if (expression == null) return Functions.Identity;
-    if (typeof expression == Types.String)
+    Boolean: typeof true,
+    Number: typeof 0,
+    String: typeof "",
+    Object: typeof {},
+    Undefined: typeof undefined,
+    Function: typeof function () { }
+  }
+
+  var FS = require?require('fs'):Types.Undefined;
+
+  var Utils =
+  {
+    CreateLambda: function (expression)
     {
-      if (expression == "")
+      if (expression == null) return Functions.Identity;
+      if (typeof expression == Types.String)
       {
-        return Functions.Identity;
+        if (expression == "")
+        {
+          return Functions.Identity;
+        }
+        else if (expression.indexOf("=>") == -1)
+        {
+          return new Function("$,$$,$$$,$$$$", "return " + expression);
+        }
+        else
+        {
+          var expr = expression.match(/^[(\s]*([^()]*?)[)\s]*=>(.*)/);
+          return new Function(expr[1], "return " + expr[2]);
+        }
       }
-      else if (expression.indexOf("=>") == -1)
-      {
-        return new Function("$,$$,$$$,$$$$", "return " + expression);
-      }
-      else
-      {
-        var expr = expression.match(/^[(\s]*([^()]*?)[)\s]*=>(.*)/);
-        return new Function(expr[1], "return " + expr[2]);
+      return expression;
+    }
+  }
+
+  var ProcessPara = function(para){
+    if(typeof para === Types.String){
+      return Utils.CreateLambda(para);
+    }else if(typeof para === Types.Function ){
+      return para;
+    }else{
+      throw new TypeError()
+    }
+  }
+
+  /**
+   * log object
+   */
+  Object.prototype.sLog = function(lambda){
+    if(console){
+      console.log(this);
+    }
+    return this;
+  }
+
+
+  /**
+   * if node.js, dumps an object to file
+   * else dumps string to console
+   * @param filename
+   */
+  Object.prototype.sDumps = function(filename){
+    if(FS){
+      FS.writeFileSync(filename, JSON.stringify(this))
+    }else{
+      if(console){
+        console.log(JSON.stringify(this))
       }
     }
-    return expression;
-  },
+    return this;
+  }
 
-  Compare: function (a, b)
+
+  /**
+   * node.js only, loads an object from file
+   * @param filename
+   */
+  Object.prototype.sLoads= function(filename){
+    var rtn;
+    if(FS){
+      rtn = JSON.parse(FS.readFileSync(filename));
+    }
+    return rtn;
+  }
+
+  /**
+   * filter array
+   * @param lambda
+   * @returns {Array}
+   */
+  Array.prototype.sFilter = function(lambda /*, thisp */)
   {
-    return (a === b) ? 0
-      : (a > b) ? 1
-      : -1;
-  },
-
-  Dispose: function (obj)
-  {
-    if (obj != null) obj.Dispose();
-  }
-}
-
-/**
- * log a object
- */
-Object.prototype.sLog = function(){
-  if(console){
-    console.log(this);
-  }
-}
-
-
-/**
- * for node.js, dumps an object to file
- * @param filename
- */
-Object.prototype.sDump = function(filename){
-  if(require){
-    var fs = require('fs');
-    fs.writeFileSync(filename, JSON.stringify(this))
-  }
-}
-
-/**
- * filter an array
- * @param fun
- * @returns {Array}
- */
-Array.prototype.sFilter = function(fun /*, thisp */)
-{
-  "use strict";
-
-  if (this == null)
-    throw new TypeError();
-
-  var t = Object(this);
-  var len = t.length >>> 0;
-  if(typeof  fun === Types.String){
-    fun = Utils.CreateLambda(fun)
-  }
-  if (typeof fun != Types.Function)
-    throw new TypeError();
-
-  var res = [];
-  var thisp = arguments[1];
-  for (var i = 0; i < len; i++)
-  {
-    if (i in t)
+    "use strict";
+    if (this == null)
+      throw new TypeError();
+    var t = Object(this);
+    var len = t.length >>> 0;
+    lambda = ProcessPara(lambda);
+    var res = [];
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
     {
-      var val = t[i]; // in case fun mutates this
-      if (fun.call(thisp, val, i, t))
-        res.push(val);
+      if (i in t)
+      {
+        var val = t[i]; // in case lambda mutates this
+        if (lambda.call(thisp, val, i, t))
+          res.push(val);
+      }
     }
+    return res;
+  };
+
+
+  /**
+   * do something with the elements of the array
+   * @param lambda
+   */
+  Array.prototype.sMap = function(lambda) {
+    return this.map(ProcessPara(lambda));
+  };
+  
+  Array.prototype.sReduce = function(lambda){
+    lambda = ProcessPara(lambda)
+    return this.reduce(lambda);
   }
 
-  return res;
-};
+  /**
+   * Diffuse the array, Make it longer
+   * @param lambda
+   * @param thisArg
+   */
+  Array.prototype.sDiffuse = function(lambda) {
 
+    var T, k;
 
-/**
- * traverse an array
- * @param callback
- * @param thisArg
- */
-Array.prototype.sForEach = function forEach( callback, thisArg ) {
-
-  var T, k;
-
-  if ( this == null ) {
-    throw new TypeError( "this is null or not defined" );
-  }
-
-  // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-  var O = Object(this);
-
-  // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
-  // 3. Let len be ToUint32(lenValue).
-  var len = O.length >>> 0; // Hack to convert O.length to a UInt32
-
-  // 4. If IsCallable(callback) is false, throw a TypeError exception.
-  // See: http://es5.github.com/#x9.11
-  if(typeof callback === Types.String){
-    callback = Utils.CreateLambda(callback)
-  }
-  if (typeof callback != Types.Function)
-    throw new TypeError();
-
-  // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-  if ( thisArg ) {
-    T = thisArg;
-  }
-
-  // 6. Let k be 0
-  k = 0;
-
-  // 7. Repeat, while k < len
-  while( k < len ) {
-
-    var kValue;
-
-    // a. Let Pk be ToString(k).
-    //   This is implicit for LHS operands of the in operator
-    // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
-    //   This step can be combined with c
-    // c. If kPresent is true, then
-    if ( Object.prototype.hasOwnProperty.call(O, k) ) {
-
-      // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-      kValue = O[ k ];
-
-      // ii. Call the Call internal method of callback with T as the this value and
-      // argument list containing kValue, k, and O.
-      callback.call( T, kValue, k, O );
+    if ( this == null ) {
+      throw new TypeError( "this is null or not defined" );
     }
-    // d. Increase k by 1.
-    k++;
-  }
-  // 8. return undefined
-};
 
+    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+    var O = Object(this);
+
+    lambda = ProcessPara(lambda);
+
+    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+
+    // 4. If IsCallable(lambda) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+
+    var res = [];
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if ( thisArg ) {
+      T = thisArg;
+    }
+
+    // 6. Let k be 0
+    k = 0;
+
+    // 7. Repeat, while k < len
+    while( k < len ) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if ( Object.prototype.hasOwnProperty.call(O, k) ) {
+
+        // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+        kValue = O[ k ];
+
+        // ii. Call the Call internal method of lambda with T as the this value and
+        // argument list containing kValue, k, and O.
+        lambda.call( T, kValue, k, O );
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined
+  };
+
+})();
